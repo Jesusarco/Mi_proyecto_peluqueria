4f325f3e912bb9f113cc5b83db74cbc3d0485984
Jesusarco <jesusmasy@gmail.com>
2026-05-26 19:59:13 +0200

    Delete mi_proyecto_peluqueria directory

--- mi_proyecto_peluqueria/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Punto de entrada de la aplicación.
- * Redirige automáticamente a la página de inicio pública.
- * El inicio de sesión se realiza desde el enlace en la cabecera.
- */
-header("Location: user/inicio.php");
-exit();
-?>
